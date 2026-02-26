aa5317ddf049bf3c2a0e1c0306ae87094ea25289
RiseForever <riseforever@riseforever.cn>
2026-02-26 09:16:53 +0800

    修正依赖

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 export default {
-  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'subject-empty': [0, 'never'],
+    'type-empty': [0, 'never'],
+    'subject-case': [0, 'always', 'lower-case'],
+  },
 }
